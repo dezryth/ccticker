5e98fcd92312c11a54e7cbf3062c6a6759cb7336
Scott Christian <Scott Christian>
2017-07-23 21:55:45 -0500

    Add project files.

---
+++ CCTicker/CCTickerFrm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CCTicker
+{
+  partial class CCTickerFrm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.lblDecredUSDValue = new System.Windows.Forms.Label();
+      this.lblDecredUSDValueDisplay = new System.Windows.Forms.Label();
+      this.tmrDecred = new System.Windows.Forms.Timer(this.components);
+      this.lblBitcoinUSDValue = new System.Windows.Forms.Label();
+      this.lblBitcoinUSDValueDisplay = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // lblDecredUSDValue
+      // 
+      this.lblDecredUSDValue.AutoSize = true;
+      this.lblDecredUSDValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblDecredUSDValue.ForeColor = System.Drawing.Color.Lime;
+      this.lblDecredUSDValue.Location = new System.Drawing.Point(7, 10);
+      this.lblDecredUSDValue.Name = "lblDecredUSDValue";
+      this.lblDecredUSDValue.Size = new System.Drawing.Size(207, 13);
+      this.lblDecredUSDValue.TabIndex = 0;
+      this.lblDecredUSDValue.Text = "DECRED\'S CURRENT USD VALUE";
+      // 
+      // lblDecredUSDValueDisplay
+      // 
+      this.lblDecredUSDValueDisplay.AutoSize = true;
+      this.lblDecredUSDValueDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblDecredUSDValueDisplay.ForeColor = System.Drawing.Color.Red;
+      this.lblDecredUSDValueDisplay.Location = new System.Drawing.Point(12, 34);
+      this.lblDecredUSDValueDisplay.Name = "lblDecredUSDValueDisplay";
+      this.lblDecredUSDValueDisplay.Size = new System.Drawing.Size(196, 46);
+      this.lblDecredUSDValueDisplay.TabIndex = 2;
+      this.lblDecredUSDValueDisplay.Text = "$$$$$$$$";
+      // 
+      // tmrDecred
+      // 
+      this.tmrDecred.Interval = 15000;
+      this.tmrDecred.Tick += new System.EventHandler(this.tmrDecred_Tick);
+      // 
+      // lblBitcoinUSDValue
+      // 
+      this.lblBitcoinUSDValue.AutoSize = true;
+      this.lblBitcoinUSDValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblBitcoinUSDValue.ForeColor = System.Drawing.Color.Lime;
+      this.lblBitcoinUSDValue.Location = new System.Drawing.Point(8, 99);
+      this.lblBitcoinUSDValue.Name = "lblBitcoinUSDValue";
+      this.lblBitcoinUSDValue.Size = new System.Drawing.Size(206, 13);
+      this.lblBitcoinUSDValue.TabIndex = 3;
+      this.lblBitcoinUSDValue.Text = "BITCOIN\'S CURRENT USD VALUE";
+      // 
+      // lblBitcoinUSDValueDisplay
+      // 
+      this.lblBitcoinUSDValueDisplay.AutoSize = true;
+      this.lblBitcoinUSDValueDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblBitcoinUSDValueDisplay.ForeColor = System.Drawing.Color.Red;
+      this.lblBitcoinUSDValueDisplay.Location = new System.Drawing.Point(12, 126);
+      this.lblBitcoinUSDValueDisplay.Name = "lblBitcoinUSDValueDisplay";
+      this.lblBitcoinUSDValueDisplay.Size = new System.Drawing.Size(196, 46);
+      this.lblBitcoinUSDValueDisplay.TabIndex = 4;
+      this.lblBitcoinUSDValueDisplay.Text = "$$$$$$$$";
+      // 
+      // CCTickerFrm
+      // 
+      this.BackColor = System.Drawing.Color.Black;
+      this.ClientSize = new System.Drawing.Size(223, 211);
+      this.Controls.Add(this.lblBitcoinUSDValueDisplay);
+      this.Controls.Add(this.lblBitcoinUSDValue);
+      this.Controls.Add(this.lblDecredUSDValueDisplay);
+      this.Controls.Add(this.lblDecredUSDValue);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+      this.Name = "CCTickerFrm";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label lblDecredUSDValue;
+    private System.Windows.Forms.Label lblDecredUSDValueDisplay;
+    private System.Windows.Forms.Timer tmrDecred;
+    private System.Windows.Forms.Label lblBitcoinUSDValue;
+    private System.Windows.Forms.Label lblBitcoinUSDValueDisplay;
+  }
+}
+
---
+++ CCTicker/CCTickerFrm.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static CCTicker.CCTickerFrm;
+
+namespace CCTicker
+{
+  public partial class CCTickerFrm : Form
+  {
+    public CCTickerFrm()
+    {
+      InitializeComponent();
+      tmrDecred.Start();
+      RequestCryptoInfo();
+    }
+
+    [Serializable]
+    public class CryptoStats
+    {
+      [JsonProperty("id")]
+      public string id { get; set; }
+      [JsonProperty("name")]
+      public string name { get; set; }
+      [JsonProperty("symbol")]
+      public string symbol { get; set; }
+      [JsonProperty("rank")]
+      public int rank { get; set; }
+      [JsonProperty("price_usd")]
+      public float price_usd { get; set; }
+      [JsonProperty("price_btc")]
+      public float price_btc { get; set; }
+      [JsonProperty("24h_volume_usd")]
+      public float day_volume_usd { get; set; }
+      [JsonProperty("market_cap_usd")]
+      public float market_cap_usd { get; set; }
+      [JsonProperty("total_supply")]
+      public float total_supply { get; set; }
+      [JsonProperty("percent_change_1h")]
+      public float percent_change_1h { get; set; }
+      [JsonProperty("percent_change_24h")]
+      public float percent_change_24h { get; set; }
+      [JsonProperty("percent_change_7d")]
+      public float percent_change_7d { get; set; }
+      [JsonProperty("last_updated")]
+      public double last_updated { get; set; }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tmrDecred_Tick(object sender, EventArgs e)
+    {
+      RequestCryptoInfo();
+    }
+
+    private void RequestCryptoInfo()
+    {
+      using (WebClient wc = new WebClient())
+      {
+        string decredjson = wc.DownloadString("https://api.coinmarketcap.com/v1/ticker/decred");
+        string bitcoinjson = wc.DownloadString("https://api.coinmarketcap.com/v1/ticker/bitcoin");
+
+
+        decredjson = StripArrayCharacters(decredjson);
+        bitcoinjson = StripArrayCharacters(bitcoinjson);
+
+        CryptoStats decred = JsonConvert.DeserializeObject<CryptoStats>(decredjson);
+        CryptoStats bitcoin = JsonConvert.DeserializeObject<CryptoStats>(bitcoinjson);
+
+        lblDecredUSDValueDisplay.Text = "$" + decred.price_usd.ToString();
+        lblBitcoinUSDValueDisplay.Text = "$" + bitcoin.price_usd.ToString();
+      }
+    }
+
+    private string StripArrayCharacters(string json)
+    {
+      json = json.Replace("[", "");
+      json = json.Replace("]", "");
+      return json;
+    }
+  }
+}
